91040d5502b13aac4d59ee9651765f49c03d82aa
NataliaHarashchuk <159415965+NataliaHarashchuk@users.noreply.github.com>
2025-05-16 15:12:41 +0300

    task4 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import './App.scss'
 
 function App() {
-    return (
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const usernameInput = form.elements.namedItem("username") as HTMLInputElement;
+
+    console.log(usernameInput.value);
+  };
+
+  return (
     <>
-      
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Введи ім'я" />
+        <button type="submit">Відправити</button>
+      </form>
     </>
   )
 }
