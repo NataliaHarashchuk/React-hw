571e7a76b6c2906879aa61a940655be2626c8fe4
NataliaHarashchuk <159415965+NataliaHarashchuk@users.noreply.github.com>
2025-05-16 14:26:07 +0300

    task1 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import React from 'react';
+import './App.scss'
+import picture from './assets/images/lama.jpg'
 
 function App() {
-    return (
-    <>
-      
-    </>
+  return (
+    <div className="simple-section">
+      <h1>Привіт, це заголовок сторінки!</h1>
+      <p>Це параграф з текстом. Тут можна описати щось цікаве про сторінку або розповісти більше деталей.</p>
+      <img src={picture} alt="Якесь фото" />
+    </div>
   )
+
+  return React.createElement(
+    "div",
+    { className: "simple-section" },
+    React.createElement("h1", null, "Привіт, це заголовок сторінки!"),
+    React.createElement("p", null, "Це параграф з текстом. Тут можна описати щось цікаве про сторінку або розповісти більше деталей."),
+    React.createElement("img", { src: picture, alt: "Якесь фото" })
+  );
 }
 
 export default App
